89572eb8969ef3579790fd91917e10c6e514974c
kwr760 <kroy760@gmail.com>
2021-05-12 17:34:40 -0700

    deploy

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,12 +5,16 @@ repo=${1}
 deployment=${2}
 type=${3}
 
+source ${scripts}/${repo}/env/${type}
+
 echo "installing ${repo} as ${deployment}"
+echo cd ${LOCATION}/${deployment}
 cd ${LOCATION}/${deployment}
+pwd
 
 echo "yarn install"
 yarn install
-
+pwd
 echo "copy .env"
 cp ${scripts}/${repo}/dotenv/${type}/${deployment} .env
 
@@ -19,3 +23,4 @@ rm /etc/nginx/sites-enabled/nginx.http.conf
 ln -s /etc/nginx/sites-available/nginx.http.conf /etc/nginx/sites-enabled/nginx.http.conf
 rm /etc/nginx/sites-enabled/nginx.https.conf
 ln -s /etc/nginx/sites-available/nginx.https.${deployment}.conf /etc/nginx/sites-enabled/nginx.https.conf
+
