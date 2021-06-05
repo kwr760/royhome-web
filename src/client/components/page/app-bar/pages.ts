b06372589413e98b31dd9efc49241395e801a491
kwr760 <kroy760@gmail.com>
2021-06-04 20:47:43 -0700

    WEB-173 Add icon for menu

--- src/client/components/page/app-bar/pages.ts
+++ src/client/components/page/app-bar/pages.ts
@@ -1,24 +1,30 @@
 import { TabPageType } from '../../../../types/pages.types';
 
+import { FiInfo, FiBookOpen, FiAlertTriangle, FiLayers } from 'react-icons/fi';
+
 export const pages: TabPageType[] = [
   {
     name: 'Resume',
     path: '/',
+    icon: FiLayers,
     tab: 0,
   },
   {
     name: 'About',
     path: '/about',
+    icon: FiInfo,
     tab: 1,
   },
   {
     name: 'Author',
     path: '/author',
+    icon: FiBookOpen,
     tab: 2,
   },
   {
     name: 'Work In Progress',
     path: '/tictactoe',
+    icon: FiAlertTriangle,
     role: 'engineer',
     authenticated: true,
     tab: 3,
